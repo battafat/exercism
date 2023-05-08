36210f28dbb089a667b4cbdd9589005ffd25a169
timothy battafarano <tim@deepsprings.edu>
2023-05-08 13:34:52 -0500

    first commit

---
+++ lib/leap.dart
@@ -0,0 +1,25 @@
+class Leap {
+  bool leapYear(int year) {
+
+    if (year % 100 == 0){
+      if (year % 400 == 0){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+    else if (year % 4 == 0){
+      return true;
+      }
+
+    else{
+      return false;
+    }  
+  }
+}
+
+
+
+
+
